ba4816e03f271ad64017c73889bdc6884a2a4aff
Arjun Narayanan <arjun.narayanan91@gmail.com>
2020-05-10 09:40:26 -0700

    Testing for hybrid coupling should now be triggered automatically

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,3 +3,7 @@ using SafeTestsets
 @safetestset "Test Local Operator" begin
     include("test_local_operator.jl")
 end
+
+@safetestset "Test Local Hybrid Operator" begin
+    include("test_local_hybrid_coupling.jl")
+end
--- test/test_local_hybrid_coupling.jl
+++ test/test_local_hybrid_coupling.jl
@@ -1,7 +1,6 @@
 using Test, StaticArrays, LinearAlgebra
 using CartesianMesh, ImplicitDomainQuadrature
 import ImplicitDomainQuadrature: extend
-using Revise
 using HDGElasticity
 
 
