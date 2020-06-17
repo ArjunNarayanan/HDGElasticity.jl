9506fea2803702fc5696f3ddec43935f2b80af4b
Arjun Narayanan <arjun.narayanan91@gmail.com>
2020-06-17 16:46:41 -0700

    Going to change reference_cell to return an IntervalBox instead

--- src/fit_interface_hybrid_element.jl
+++ src/fit_interface_hybrid_element.jl
@@ -6,11 +6,33 @@ function roots_of_restrictions(funcs,xL,xR)
     return roots,indices
 end
 
+function extend_to_face(x,faceid,xL,xR)
+    if faceid == 1
+        return extend(x,2,xL)
+    elseif faceid == 2
+        return extend(x,1,xR)
+    elseif faceid == 3
+        return extend(x,2,xR)
+    elseif faceid == 4
+        return extend(x,1,xL)
+    else
+        throw(ArgumentError("Expected faceid ∈ {1,2,3,4}, got faceid = $faceid"))
+    end
+end
+
+function extend_face_roots(dim,roots,indices,xL,xR)
+    numroots = length(indices)
+    extended_roots = zeros(dim,numroots)
+    for (count,idx) in enumerate(indices)
+        extended_roots[:,count] .= extend_to_face(roots[idx],idx,xL,xR)
+    end
+    return extended_roots
+end
 
 function element_face_intersections(poly::InterpolatingPolynomial{dim},
     coeffs) where {dim}
 
     xL,xR = reference_cell(dim)
     funcs = restrict_on_faces(poly,xL[1],xR[1])
-
+    roots,indices = roots_of_restrictions(funcs,xL[1],xR[1])
 end
--- test/test_fit_interface_hybrid_element.jl
+++ test/test_fit_interface_hybrid_element.jl
@@ -4,6 +4,7 @@ using ImplicitDomainQuadrature
 using CartesianMesh
 using Revise
 using HDGElasticity
+import ImplicitDomainQuadrature: extend
 
 function allequal(u,v)
     return all(u .== v)
@@ -31,4 +32,14 @@ coeffs = reshape(distance_function(coords,xc),NF,:)
 update!(poly,coeffs[:,1])
 funcs = HDGElasticity.restrict_on_faces(poly,-1.,1.)
 
-roots,flags = HDGElasticity.roots_of_restrictions(funcs,-1.,1.)
+roots,indices = HDGElasticity.roots_of_restrictions(funcs,-1.,1.)
+@test roots[1][1] == 0.5
+@test roots[3][1] == 0.5
+@test allequal(indices,[1,3])
+
+@test allequal(HDGElasticity.extend_to_face(roots[1],1,-1.,1.),[0.5,-1.0])
+@test allequal(HDGElasticity.extend_to_face(roots[1],2,-1.,1.),[1.0,0.5])
+@test allequal(HDGElasticity.extend_to_face(roots[1],3,-1.,1.),[0.5,+1.0])
+@test allequal(HDGElasticity.extend_to_face(roots[1],4,-1.,1.),[-1.0,0.5])
+
+extended_roots = HDGElasticity.extend_face_roots(2,roots,indices,-1.,1.)
